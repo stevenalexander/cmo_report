c8fc69bdf4f39c82baf248026fc84e8126af2227
Steven Alexander <steven.william.alexander@googlemail.com>
2013-06-09 23:24:35 +0100

    added basic generator

--- _plugins/data_generator.rb
+++ _plugins/data_generator.rb
@@ -3,8 +3,24 @@ module Jekyll
     safe true
     priority :low
 
+    GRAPHS_FOLDER = "_plugins/data/CMO Report - Graphs"
+    MAPS_FOLDER   = "_plugins/data/CMO Report - Maps"
+
+    GRAPHS_OUTPUT_FOLDER = "graphs"
+    MAPS_OUTPUT_FOLDER   = "maps"
+
     def generate(site)
-      puts "generate data from data files"
+      graphs_file_paths = get_all_data_file_paths(GRAPHS_FOLDER)
+      maps_file_paths   = get_all_data_file_paths(MAPS_FOLDER)
+
+      graphs_file_paths.each { |path| parse_data_file(path, GRAPHS_OUTPUT_FOLDER) }
+      maps_file_paths.each   { |path| parse_data_file(path, MAPS_OUTPUT_FOLDER) }
+    end
+
+    def get_all_data_file_paths(base_folder)
+    end
+
+    def parse_data_file(file_path, output_folder)
     end
   end
 end
--- _spec/data_generator_spec.rb
+++ _spec/data_generator_spec.rb
@@ -11,7 +11,18 @@ describe DataGenerator do
 
   describe "generate" do
     it "should generate the site from the data" do
-      true.should be_true
+      data_generator = DataGenerator.new
+      data_generator.stub(:get_all_data_file_paths).and_return(["file_path1","file_path2"])
+      data_generator.stub(:parse_data_file)
+
+      data_generator.should_receive(:get_all_data_file_paths).with(DataGenerator::GRAPHS_FOLDER).once.ordered
+      data_generator.should_receive(:get_all_data_file_paths).with(DataGenerator::MAPS_FOLDER).once.ordered
+      data_generator.should_receive(:parse_data_file).with("file_path1", DataGenerator::GRAPHS_OUTPUT_FOLDER)
+      data_generator.should_receive(:parse_data_file).with("file_path2", DataGenerator::GRAPHS_OUTPUT_FOLDER)
+      data_generator.should_receive(:parse_data_file).with("file_path1", DataGenerator::MAPS_OUTPUT_FOLDER)
+      data_generator.should_receive(:parse_data_file).with("file_path2", DataGenerator::MAPS_OUTPUT_FOLDER)
+
+      data_generator.generate(@path)
     end
   end
 end
