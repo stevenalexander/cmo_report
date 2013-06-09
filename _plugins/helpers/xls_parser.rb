1a7e1fa01dbb211c02c43bc38333cbb92e1ec6c7
Steven Alexander <steven.william.alexander@googlemail.com>
2013-06-09 22:43:19 +0100

    parse xls file

--- _plugins/helpers/xls_parser.rb
+++ _plugins/helpers/xls_parser.rb
@@ -1,9 +1,115 @@
 # encoding: utf-8
 require 'spreadsheet'
+require 'json'
 
 class XlsParser
+  attr_accessor :xls
   def initialize file_path
+    @file_name = File.basename(file_path)
     @xls = Spreadsheet.open(file_path)
-    puts @xls.worksheet(0).row(1)[1]
   end
+
+  def is_summary_sheet(worksheet)
+    worksheet.row(1).join(',').include? "Summary sheet"
+  end
+
+  def is_data_sheet(worksheet)
+    worksheet.row(0).join(',').include? "CMO Annual Report"
+  end
+
+  def print_sheets
+    begin
+      (0..@xls.sheet_count).each do |i|
+        puts "sheet #{i}"
+        j = 0
+        sheet = @xls.worksheet(i)
+        sheet.each do |row|
+          puts "#{j}: #{row.join(',')}"
+          j += 1
+       end
+      end
+    rescue
+    end
+  end
+
+  def generate_json(json_file_path)
+    content = {
+      :orginal_file_name => @file_name,
+      :summary => nil,
+      :data => []
+    }
+
+    (0..@xls.sheet_count).each do |i|
+      sheet = @xls.worksheet(i)
+      next if sheet.nil?
+
+      if is_summary_sheet(sheet)
+        content[:summary] = generate_summary_hash(sheet)
+      elsif is_data_sheet(sheet)
+        content[:data] << generate_data_hash(sheet)
+      end
+    end
+
+    File.open(json_file_path, 'w+') {|f| f.write(content.to_json) }
+  end
+
+  def generate_summary_hash(worksheet)
+    summary = {}
+
+    worksheet.each do |row|
+      next if row.nil? || row.length < 3
+      parse_metadata_row(row, summary)
+    end
+
+    summary
+  end
+
+  def generate_data_hash(worksheet)
+    data = { :metadata => {}, :data => [] }
+    data_sheet_ons_column_header = "ONS 9 character code"
+    found_data_column_header = false
+    headers = []
+
+    worksheet.each do |row|
+      next if row.nil?
+      is_header_row = false
+
+      if !found_data_column_header && row.join(',').include?(data_sheet_ons_column_header)
+        found_data_column_header = true
+        is_header_row = true
+        headers = row.map { |cell| cell.to_s.strip }
+      end
+
+      if !found_data_column_header
+        parse_metadata_row(row ,data[:metadata], 2)
+      elsif !is_header_row
+        data[:data] << parse_data_row(row, headers)
+      end
+    end
+
+    data
+  end
+
+  def parse_metadata_row(row, hash, data_start_index = 1)
+    return nil if row.nil? && row.length < data_start_index+2
+
+    key = row[data_start_index]
+    value = row[data_start_index+1]
+    if !key.nil?   && !key.to_s.empty? &&
+       !value.nil? && !value.to_s.empty?
+      hash[key] = value
+    end
+  end
+
+  def parse_data_row(row, headers)
+    return nil if row.nil? && row.length < headers.length
+    data_row = {}
+
+    headers.each_with_index do |header, i|
+      data_row[header] = row[i] if !header.empty?
+    end
+
+    data_row
+  end
+
 end
--- _spec/helpers/xls_parser_spec.rb
+++ _spec/helpers/xls_parser_spec.rb
@@ -1,17 +1,222 @@
 # encoding: utf-8
 require_relative "../../_plugins/helpers/xls_parser.rb"
+require 'json'
+require 'spreadsheet'
 
 SAMPLE_XLS_GRAPH_PATH = "_spec/test_data/graphs_38_C1_G1.xls"
 SAMPLE_XLS_MAP_PATH   = "_spec/test_data/maps_36_C1_M1.xls"
 
 describe XlsParser do
-  before(:each) do
-    @xls_parser = XlsParser.new(SAMPLE_XLS_GRAPH_PATH)
+  before :all do
+    @graph_parser = XlsParser.new(SAMPLE_XLS_GRAPH_PATH)
+    @graph_summary_worksheet = @graph_parser.xls.worksheet(0)
+
+    @graph_data_sheet_worksheet = @graph_parser.xls.worksheet(1)
+    @graph_data_sheet_2_worksheet = @graph_parser.xls.worksheet(2)
+
+    @map_parser = XlsParser.new(SAMPLE_XLS_MAP_PATH)
+    @map_data_sheet_worksheet = @map_parser.xls.worksheet(0)
+
+    #@map_parser.print_sheets
   end
 
   describe "#new" do
     it "takes a file path parameter and returns a XlsParser object" do
-      @xls_parser.should be_an_instance_of XlsParser
+      XlsParser.new(SAMPLE_XLS_GRAPH_PATH).should be_an_instance_of XlsParser
+    end
+  end
+  describe "#xls" do
+    it "returns a workbook object" do
+      parser = XlsParser.new(SAMPLE_XLS_GRAPH_PATH)
+      parser.xls.should be_an_instance_of Spreadsheet::Excel::Workbook
+    end
+  end
+
+  describe "is_summary_sheet" do
+    it "returns true for a summary sheet" do
+      @graph_parser.is_summary_sheet(@graph_summary_worksheet).should be_true
+    end
+    it "returns false for a data sheet" do
+      @graph_parser.is_summary_sheet(@graph_data_sheet_worksheet).should be_false
+      @graph_parser.is_summary_sheet(@graph_data_sheet_2_worksheet).should be_false
+      @map_parser.is_summary_sheet(@map_data_sheet_worksheet).should be_false
+    end
+  end
+
+  describe "is_data_sheet" do
+    it "returns true for a data sheet" do
+      @graph_parser.is_data_sheet(@graph_data_sheet_worksheet).should be_true
+      @graph_parser.is_data_sheet(@graph_data_sheet_2_worksheet).should be_true
+      @map_parser.is_data_sheet(@map_data_sheet_worksheet).should be_true
+    end
+    it "returns false for a summary sheet" do
+      @graph_parser.is_data_sheet(@graph_summary_worksheet).should be_false
+    end
+  end
+
+  describe "generate_json" do
+    context "sample graph with mocks" do
+      before :each do
+        @parser = XlsParser.new(SAMPLE_XLS_GRAPH_PATH)
+        @json_file_path = "graph.json"
+
+        @parser.stub(:generate_summary_hash).and_return({ "Illustration reference" => "38  C1 G1" })
+        @parser.stub(:generate_data_hash).and_return({ "name" => "Data sheet 1" })
+
+        @parser.generate_json(@json_file_path)
+        @content = File.read(@json_file_path) if File.exists?(@json_file_path)
+        @json = JSON.parse(@content) if !@content.nil?
+      end
+      it "generates a json file" do
+        File.exists?(@json_file_path).should be_true
+      end
+      it "content is json" do
+        @json.nil?.should be_false
+      end
+      it "contains original filename" do
+        @json["orginal_file_name"].should eq "graphs_38_C1_G1.xls"
+      end
+      it "contains summary worksheet contents" do
+        @json.has_key?("summary").should be_true
+        @json["summary"]["Illustration reference"].should eq "38  C1 G1"
+      end
+      it "contains the two Data sheet worksheet contents" do
+        @json.has_key?("data").should be_true
+        (@json["data"].length == 2).should be_true
+        @json["data"][0]["name"].should eq "Data sheet 1"
+        @json["data"][1]["name"].should eq "Data sheet 1"
+      end
+      after :each do
+        File.delete(@json_file_path) if File.exists?(@json_file_path)
+      end
+    end
+    context "sample map with mocks" do
+      before :each do
+        @parser = XlsParser.new(SAMPLE_XLS_MAP_PATH)
+        @json_file_path = "map.json"
+
+        @parser.stub(:generate_summary_hash).and_return({})
+        @parser.stub(:generate_data_hash).and_return({ "name" => "Data sheet 1" })
+
+        @parser.generate_json(@json_file_path)
+        @content = File.read(@json_file_path) if File.exists?(@json_file_path)
+        @json = JSON.parse(@content) if !@content.nil?
+      end
+      it "generates a json file" do
+        File.exists?(@json_file_path).should be_true
+      end
+      it "content is json" do
+        @json.nil?.should be_false
+      end
+      it "contains original filename" do
+        @json["orginal_file_name"].should eq "maps_36_C1_M1.xls"
+      end
+      it "contains no summary worksheet contents" do
+        @json.has_key?("summary").should be_true
+        @json["summary"].nil?.should be_true
+      end
+      it "contains the one Data sheet worksheet contents" do
+        @json.has_key?("data").should be_true
+        (@json["data"].length == 1).should be_true
+        @json["data"][0]["name"].should eq "Data sheet 1"
+      end
+      after :each do
+        File.delete(@json_file_path) if File.exists?(@json_file_path)
+      end
+    end
+  end
+
+  describe "generate_summary_hash" do
+    context "sample graph summary" do
+      before :each do
+        @summary_hash = @graph_parser.generate_summary_hash(@graph_summary_worksheet)
+      end
+      it "should include the summary information in the form of key value pairs" do
+        @summary_hash["Illustration reference"].should eq "38  C1 G1"
+        @summary_hash["x-axis label"].should eq "Population in thousands"
+      end
+    end
+  end
+
+  describe "generate_data_hash" do
+    context "sample graph data sheet" do
+      before :each do
+        @data_hash = @graph_parser.generate_data_hash(@graph_data_sheet_worksheet)
+      end
+      it "should include the metadata from the data sheet in the form of key value pairs" do
+        @data_hash.has_key?(:metadata).should be_true
+        @data_hash[:metadata].keys.length.should eq 20
+        @data_hash[:metadata]["Chapter:"].should eq "Chapter 1: Demography"
+        @data_hash[:metadata]["Significance:"].should eq "Complete if applicable - Significantly better than the England average, Significantly worse than the England average, No significance can be calculated"
+      end
+      it "should include the data from the data sheet table section in the form an array of hashes" do
+        @data_hash.has_key?(:data).should be_true
+        @data_hash[:data].length.should eq 76
+        @data_hash[:data][0].keys.should eq [
+          "ONS 9 character code",
+          "Area Code",
+          "Area Name",
+          "Period",
+          "Gender",
+          "Age group",
+          "Ethnic group",
+          "Deprivation grouping",
+          "Numerator",
+          "Denominator",
+          "Indicator value",
+          "Lower 95% CI",
+          "Upper 95% CI",
+          "Significance"
+        ]
+
+        @data_hash[:data][0]["Area Code"].should eq "Eng"
+        @data_hash[:data][0]["Period"].should eq "2010"
+        @data_hash[:data][0]["Age group"].should eq "0-4"
+        @data_hash[:data][0]["Indicator value"].should eq 1674.8289999999997
+
+        @data_hash[:data][1]["Area Code"].should eq "Eng"
+        @data_hash[:data][1]["Period"].should eq "2010"
+        @data_hash[:data][1]["Age group"].should eq "5-9"
+        @data_hash[:data][1]["Indicator value"].should eq 1482.958
+      end
+    end
+
+    context "sample map data sheet" do
+      before :each do
+        @data_hash = @map_parser.generate_data_hash(@map_data_sheet_worksheet)
+      end
+      it "should include the metadata from the data sheet in the form of key value pairs" do
+        @data_hash.has_key?(:metadata).should be_true
+        @data_hash[:metadata].keys.length.should eq 20
+        @data_hash[:metadata]["Chapter:"].should eq "Chapter 1: Demography"
+        @data_hash[:metadata]["Significance:"].should eq "Complete if applicable - Significantly better than the England average, Significantly worse than the England average, No significance can be calculated"
+      end
+      it "should include the data from the data sheet table section in the form an array of hashes" do
+        @data_hash.has_key?(:data).should be_true
+        @data_hash[:data].length.should eq 152
+        @data_hash[:data][0].keys.should eq [
+          "ONS 9 character code",
+          "Area Code",
+          "Area Name",
+          "Period",
+          "Gender",
+          "Age group",
+          "Ethnic group",
+          "Deprivation grouping",
+          "Numerator",
+          "Denominator",
+          "Indicator value",
+          "Lower 95% CI",
+          "Upper 95% CI",
+          "Significance"
+        ]
+
+        @data_hash[:data][0]["ONS 9 character code"].should eq "E06000001"
+        @data_hash[:data][0]["Indicator value"].should eq 973
+
+        @data_hash[:data][1]["ONS 9 character code"].should eq "E06000002"
+        @data_hash[:data][1]["Indicator value"].should eq 2643
+      end
     end
   end
 end
