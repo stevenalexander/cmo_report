92dc1bcda64c9303dcc087b4de7b5b113cd85a21
Steven Alexander <steven.william.alexander@googlemail.com>
2013-06-17 22:40:22 +0100

    added visualisation tests

---
+++ spec/javascripts/visualisations_spec.js
@@ -0,0 +1,7 @@
+describe("Visualisations", function() {
+  var util = new cmo.utils.Util();
+
+  it("util should have function inherit", function() {
+    expect(typeof util.inherit).toEqual("function");
+  });
+});
